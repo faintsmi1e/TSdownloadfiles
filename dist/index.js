5fa011b88c9353fa906025422dc8c24403fb51c6
pinosol <zonest_64@mail.ru>
2021-10-20 19:03:48 +0300

    added ts and types

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+// start the Express server
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
